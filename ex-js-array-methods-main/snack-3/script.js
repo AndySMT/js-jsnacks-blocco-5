bdaa1f3df26d0af764e6ef3ef166f07de57aebbf
Andy Simota <andyiulian.simota@gmail.com>
2024-11-15 14:58:13 +0100

    init and solved es 1-2-3

---
+++ ex-js-array-methods-main/snack-3/script.js
@@ -0,0 +1,6 @@
+let numbers = [2, 8, 4, 7, 2, 87];
+
+// Crea un nuovo array in cui tutti i numeri siano incrementati di 1
+// Risultato: [3, 9, 5, 8, 3, 88]
+
+console.log(numbers.map((numero) => numero + 1));
